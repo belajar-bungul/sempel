1f77efaa6d504693f6920591ff9c088f702575f1
arasoft <rayhanalief212@gmail.com>
2026-08-31 09:29:17 +0700

    u

--- simplify_access_management_by_ara/__manifest__.py
+++ simplify_access_management_by_ara/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Simplify Access Management ODOO By ARA',
-    'version': '19.0.0.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Extra Tools',
     'summary': 'All-in-one access control for fields, models, menus, records, & system UI, plus complete white-label debranding.',
     'author': 'ARA SOFT',
